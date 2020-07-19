94fb4fc710a6552fd3d597a252ff1eeb25fd226c
Sergey <ryabovs@yahoo.com>
2020-07-19 06:31:51 +0200

    implements basic server

---
+++ app-1/index.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const registerRoute = require("./routes/register");
+
+const port = 3000;
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
