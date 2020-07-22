79179f1e06fa65995f053c727d753e01d7109e8f
Sergey <ryabovs@yahoo.com>
2020-07-22 06:36:41 +0200

    different env setup

--- app-1/src/server.js
+++ app-1/src/server.js
@@ -1,4 +1,8 @@
-require('dotenv').config();
+const path = require('path');
+
+require('dotenv').config({
+    path: path.resolve(__dirname, `../${process.env.NODE_ENV}.env`)
+});
 
 const app = require('./app');
 const port = 3000;
