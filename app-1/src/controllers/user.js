698f2cb90d48be6b231a5d4a361cd6708bdc5f71
Sergey <ryabovs@yahoo.com>
2020-07-23 04:20:49 +0200

    adds /users POST tests and implementation

--- app-1/src/controllers/user.js
+++ app-1/src/controllers/user.js
@@ -1,7 +1,21 @@
 const User = require('../models/User');
 
-module.exports.createUser = async (req, res) => {
-    const user = await User.create(req.body);
+const DUPLICATE_KEY_ERROR_CODE = 11000;
 
-    res.send(user);
+module.exports.createUser = async (req, res) => {
+    try {
+        const user = await User.create(req.body);
+
+        res.statusCode = 201;
+        res.send(user);
+    } catch (err) {
+        if (err.code === DUPLICATE_KEY_ERROR_CODE) {
+            res.statusCode = 409;
+            res.body = 'Email already exists';
+        } else {
+            res.statusCode = 400;
+        }
+
+        res.end();
+    }
 };
--- app-1/src/tests/users.test.js
+++ app-1/src/tests/users.test.js
@@ -1,15 +1,42 @@
 const request = require('supertest');
+const connection = require('../libs/connection');
+const User = require('../models/User');
 const app = require('../app');
 
-describe('Validating /users route', () => {
-    it('Should register user', async () => {
-        const userData = { hello: 'world' };
+const newUser = {
+    email: 'email@email.email',
+    password: 'test1234'
+};
 
-        const response = await request(app)
-            .post('/users')
-            .send(userData)
-            .expect(200);
+const newInclompleteUser = {
+    email: 'email@email.email'
+};
 
-        expect(response.body).toMatchObject(userData);
+describe('/users', () => {
+    beforeAll(async () => {
+        await User.deleteMany();
+    });
+
+    afterAll(async () => {
+        await User.deleteMany({});
+        connection.close();
+    });
+
+    it('Should register user', async (done) => {
+        await request(app).post('/users').send(newUser).expect(201);
+
+        const user = await User.findOne({ email: newUser.email });
+
+        expect(user).toMatchObject(newUser);
+
+        done();
+    });
+
+    it('Should respond with 409 error when a user already exists', (done) => {
+        request(app).post('/users').send(newUser).expect(409, done);
+    });
+
+    it('Should respond with 400 error when not all data are provided', (done) => {
+        request(app).post('/users').send(newInclompleteUser).expect(400, done);
     });
 });
