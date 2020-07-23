56b959d6efd442ca2ebf536afd17f75ad29b4b4b
Sergey <ryabovs@yahoo.com>
2020-07-23 09:02:10 +0200

    implements DELETE /users route

--- app-1/src/controllers/user.js
+++ app-1/src/controllers/user.js
@@ -1,3 +1,4 @@
+const mongoose = require('mongoose');
 const User = require('../models/User');
 
 const DUPLICATE_KEY_ERROR_CODE = 11000;
@@ -18,4 +19,30 @@ module.exports.createUser = async (req, res) => {
 
         res.end();
     }
+};
+
+module.exports.deleteUser = async (req, res) => {
+    const { id } = req.params;
+
+    if (!id) {
+        res.statusCode = 400;
+
+        return res.send('User id should be provided');
+    }
+
+    if (!mongoose.isValidObjectId(id)) {
+        res.statusCode = 400;
+
+        return res.send('User id is invalid');
+    }
+
+    const user = await User.findByIdAndDelete(id);
+
+    if (!user) {
+        res.statusCode = 400;
+
+        return res.send(`User with id: ${id} was not found`);
+    }
+
+    res.end();
 };
--- app-1/src/routes/users.js
+++ app-1/src/routes/users.js
@@ -1,8 +1,9 @@
 const express = require('express');
-const { createUser } = require('../controllers/user');
+const { createUser, deleteUser } = require('../controllers/user');
 
 const router = express.Router();
 
 router.post('/users', createUser);
+router.delete('/users/:id', deleteUser);
 
 module.exports = router;
