d318b2c773c6ede1787d411173c74300fc0cdcae
Sergey <ryabovs@yahoo.com>
2020-07-24 05:39:02 +0200

    adds crypto config

--- app-1/config.js
+++ app-1/config.js
@@ -3,5 +3,10 @@ module.exports = {
         dbHost: 'localhost',
         dbName: process.env.NODE_ENV === 'test' ? 'node-test' : 'node-dev',
         debug: process.env.NODE_ENV === 'dev'
+    },
+    crypto: {
+        iterations: process.env.NODE_ENV === 'test' ? 1 : 12000,
+        length: 128,
+        digest: 'sha512'
     }
 };
