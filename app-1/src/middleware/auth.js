60f1225e7dcaf28bbc558b1be73e409ad632134f
Sergey <ryabovs@yahoo.com>
2020-07-26 05:52:28 +0200

    adds auth middleware

---
+++ app-1/src/middleware/auth.js
@@ -0,0 +1,18 @@
+const jwt = require('jsonwebtoken');
+const User = require('../models/User');
+
+module.exports.auth = async (req, res, next) => {
+    try {
+        const token = req.headers.authorization.split(' ')[1];
+        const user = await User.findOne({ 'tokens.token': token });
+        const decodedToken = jwt.verify(token, user.salt);
+
+        if (user._id.toString() !== decodedToken._id) throw new Error();
+
+        req.user = user;
+
+        next();
+    } catch (err) {
+        res.status(401).send('Token is invalid');
+    }
+};
