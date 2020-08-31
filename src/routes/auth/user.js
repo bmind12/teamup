9ddf188cd79e59f4e4bff2783086f09e241c50b1
Sergey <ryabovsergey12@gmail.com>
2020-08-31 05:43:16 +0200

    adds users/me route

--- src/routes/auth/user.js
+++ src/routes/auth/user.js
@@ -1,7 +1,7 @@
 import { compose } from 'compose-middleware';
 import send from '@polka/send-type';
-import auth from '../../../middleware/auth';
-import User from '../../../models/User';
+import auth from '../../middleware/auth';
+import User from '../../models/User';
 
 const DUPLICATE_KEY_ERROR_CODE = 11000; // TODO: violates DRY
 
